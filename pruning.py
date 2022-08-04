23ab4ee20ab93323a3b7b24014c912eb50d5bfb6
isomorphicdude <47920591+isomorphicdude@users.noreply.github.com>
2022-08-04 22:33:28 +0100

    fix class

--- pruning.py
+++ pruning.py
@@ -11,7 +11,9 @@ class pruning(object):
 
     '''Sets up a pruning experiment.'''  
 
-    def __init__(self, ds_train, ds_test, model_params,
+    def __init__(self, ds_train, ds_test, 
+                model_params,
+                train_params,
                 epochs_for_pruning = 10,
                 num_pruning = 10,
                 step_perc = 0.5):
@@ -27,8 +29,9 @@ class pruning(object):
                             - 'BatchNorm'
                             - 'Dropout'
                             - 'optimizer'
-                            - 'loss'
+                            - 'loss_fn'
                             - 'metrics'
+            - train_params: 
             - epochs: epochs to train the model before pruning
             - num_pruning: no. of rounds to prune
             - step_perc: percentage to prune
@@ -36,9 +39,16 @@ class pruning(object):
         self.ds_train = ds_train
         self.ds_test = ds_test
         self.model_params = model_params
+        self.train_params = train_params
         self.epochs_for_pruning = epochs_for_pruning
         self.num_pruning = num_pruning
         self.step_perc = step_perc    
+
+    @property
+    def batch_size(self):
+        for img,label in self.ds_train:
+            num = img.shape[0]
+        return num
 
     # TODO: add Dataset implementaion
     
@@ -69,15 +79,27 @@ class pruning(object):
     
     def prune(self):
         '''
-        Prunes the model.
+        Iteratively prunes the model.
         '''  
         iterPruning(self.makeModel,
                     self.ds_train,
                     self.ds_test,
                     self.model_params,
+                    self.train_params,
                     epochs = self.epochs_for_pruning,
                     num_pruning=self.num_pruning,
-                    step_perc=self.step_perc)
+                    step_perc=self.step_perc)  
+
+    def test_run(self):
+        '''Fits the model for testing.'''  
+        model = self.makeModel()
+        model.compile(self.model_params['optimizer'],
+                    self.model_params['loss_fn'],
+                    self.model_params['metrics'])
+        model.fit(self.ds_train,
+                batch_size = self.batch_size,
+                epochs = self.epochs_for_pruning,
+                validation_data = self.ds_test)
 
 
 
